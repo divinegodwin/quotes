d55f1984887bddcfc33e9f36502243bb98b7ab27
divinegodwin172@gmail.com <divinegodwin172@gmail.com>
2022-07-02 16:38:08 +0100

    New

---
+++ App.js
@@ -0,0 +1,17 @@
+let quotes = document.querySelector(".quotes");
+
+let author = document.querySelector(".author");
+
+let btn = document.querySelector(".btn");
+
+btn.addEventListener('click', function(){
+  
+  const url = "https://api.quotable.io/random";
+  fetch(url)
+  .then(response =>response.json())
+  .then(data =>{
+    quotes.innerHTML = data.content;
+    author.innerHTML = data.author;
+  })
+})
+
